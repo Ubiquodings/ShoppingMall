7fa556833cbabe0425391920d3afa3707219dcc9
Kangchaemin <minkang911@naver.com>
2020-08-27 22:59:20 +0900

    [0827] Day COMMIT
    ===오늘 한 작업===
    : 장바구니 버튼 누르면 장바구니로 바로 이동하도록 도와주는
    Modal 추가.
    
    ===남은 작업===
    (주문으로도 바로 가는 Modal 추가하기)
    중요! payment에 넣을 Entity 하나 더 만들기.
    2. payment CSS 찾기.

--- src/main/resources/static/js/app/userAction.js
+++ src/main/resources/static/js/app/userAction.js
@@ -20,7 +20,7 @@ var userAction = {
         });
 
 
-        $(".btn-shoplist").on('click',function(e){ // detail 페이지에서 가져와야지
+        $(".btn-shoplist").on('click',function(e){ // product-detail 페이지에서 가져와야지 & 및 product-list에서의 장바구니 버튼도.
             var productId = this.children[0].value;
             //var count = this.children[1].value;
             var count = $('#count').val();
@@ -32,7 +32,7 @@ var userAction = {
                 //data: JSON.stringify({}) // TODO 간단하게 url param 으로 대체
                 data: JSON.stringify(count)
             }).done(function(){ // 왜 안되지 ?
-                alert('ok');
+                //alert('ok'); //0827 채민: modal 테스트중이라 잠시 껐습니다!
             }).fail(function(e){
                 alert('fail '+JSON.stringify(e));
             });
@@ -65,6 +65,66 @@ var userAction = {
                 dataType: 'json',
                 contentType:'application/json; charset=utf-8',
                 data: JSON.stringify(count)
+            }).done(function(){
+                alert('ok');
+                window.location.href = window.location.href;
+            }).fail(function(e){
+                alert('fail '+JSON.stringify(e));
+            });
+
+        });
+
+        $("#btn-order-from-cart").on('click', function(e){ // detail 페이지에서 가져와야지
+            /**
+             * shopListId랑 count만 가져오기 (위에 ".btn-order-immediately-from-cart" 메소드 참고!)
+             */
+
+            /**
+             * 이건 payment에 뿌릴 배열들
+             */
+            /*var ToOrder_shopListId = [];
+            var ToOrder_shopListCount = [];*/
+            var ToOrder_shopListId =  new Array();
+            var ToOrder_shopListCount = new Array();
+            /*stringArr: String[] = [];
+            stringArr: Array<String> = [];
+            stringArr: string=[];*/
+
+            /**
+             * cart-list.mustache에서 가져온 정보들
+             */
+            var form_list = document.getElementsByClassName("formlist");
+            var shopListId = document.getElementsByClassName("cart-item-id");
+            var shopListCount = document.getElementsByClassName("amount");
+
+            if (form_list.length == 1) {
+                /*ToOrder_shopListId[0] = shopListId;
+                ToOrder_shopListCount[0] = shopListCount;*/
+                ToOrder_shopListId.push(shopListId);
+                ToOrder_shopListCount.push(shopListCount);
+            }
+            else {
+                for (var i = 0; i < form_list.length; i++) {
+                    /*ToOrder_shopListId[i] = shopListId[i].value;
+                    ToOrder_shopListCount[i] = shopListCount[i].value;*/
+                    console.log("shopListId[i].value " + shopListId[i].value);
+                    console.log("shopListCount[i].value " + shopListCount[i].value);
+                    ToOrder_shopListId.push(shopListId[i].value);
+                    ToOrder_shopListCount.push(shopListCount[i].value);
+                }
+            }
+
+            var data = {
+                "shopListId_List" : ToOrder_shopListId,
+                "shopListCount_List" : ToOrder_shopListCount
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: '/api/orders/AllfromShopList/',
+                dataType: 'json',
+                contentType:'application/json; charset=utf-8',
+                data: JSON.stringify(data)
             }).done(function(){
                 alert('ok');
                 window.location.href = window.location.href;
